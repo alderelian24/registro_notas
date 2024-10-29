1679e5f41adcfa0108c89e87e550d2499f54e9db
alderelian24 <155692816+alderelian24@users.noreply.github.com>
2024-10-29 00:28:57 -0500

    actualiza style.css

--- setNewPassword.php
+++ setNewPassword.php
@@ -7,20 +7,22 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <h1>Set New Password</h1>
-    </header>
-    <main>
-        <form action="includes/setNewPassword.inc.php" method="post">
-            <input type="password" name="pwd" placeholder="New Password">
-            <input type="password" name="pwd-repeat" placeholder="Repeat New Password">
-            <input type="hidden" name="selector" value="<?php echo $_GET["selector"] ?>">
-            <input type="hidden" name="validator" value="<?php echo $_GET["validator"] ?>">
-            <button type="submit" name="set-new-password-submit">Set New Password</button>
-        </form>
-    </main>
-    <footer>
-        <p>&copy; 2020</p>
-    </footer>
+    <div class="container">
+        <header class="header">
+            <h1 class="header__title">Set New Password</h1>
+        </header>
+        <main class="main">
+            <form action="includes/setNewPassword.inc.php" method="post" class="form">
+                <input type="password" name="pwd" placeholder="New Password" class="form__input">
+                <input type="password" name="pwd-repeat" placeholder="Repeat New Password" class="form__input">
+                <input type="hidden" name="selector" value="<?php echo $_GET["selector"] ?>" class="form__input">
+                <input type="hidden" name="validator" value="<?php echo $_GET["validator"] ?>" class="form__input">
+                <button type="submit" name="set-new-password-submit" class="form__button">Set New Password</button>
+            </form>
+        </main>
+        <footer class="footer">
+            <p class="footer__text">&copy; 2020</p>
+        </footer>
+    </div>
 </body>
 </html>
