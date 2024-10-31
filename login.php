a9c999cdc59910c9cd4df081363bb3b0ed51f38c
LopezALexis10 <168349529+LopezALexis10@users.noreply.github.com>
2024-10-31 17:20:54 -0500

    login1.php

--- login.php
+++ login.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="/styles3.css?v=1">
 </head>
 <body>
-    
+    <header>
+        <div class="logo">NOTAS UTP</div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">new account</a>
+            <a href="#" class="login-button">login</a>
+        </nav>
+    </header>
+    <main>
+        <div class="form-container">
+            <h2>Login</h2>
+            <form action="login2.php" method="POST">
+                <label for="email">Please enter Email</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="password">Please enter password</label>
+                <input type="password" id="password" name="password" required>
+
+                <button type="submit">LOGIN</button>
+            </form>
+            <p>Don't have an account? <a href="#">Sign up</a></p>
+        </div>
+    </main>
 </body>
 </html>
